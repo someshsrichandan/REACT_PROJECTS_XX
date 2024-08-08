4dace54f232e60a323672f089840182f471f2148
somesh <someshsrichandan@gmail.com>
2024-08-08 23:57:11 +0530

    update weather app

--- weather_app/src/components/Weather.js
+++ weather_app/src/components/Weather.js
@@ -3,7 +3,7 @@ import Day from '../assets/bg.jpg';
 export const Weather = () => {
     return (
         <div 
-        className="w-screen h-screen flex flex-row justify-center"
+        className="w-screen h-screen flex flex-col justify-center items-center"
         style={{ 
             backgroundImage: `url(${Day})`, 
             backgroundSize: 'cover', 
@@ -11,8 +11,16 @@ export const Weather = () => {
             filter: 'brightness(0.9)'  // Adjust the brightness value as needed
         }}
     >
-            <div className="w-[70rem] h-52 m-20 backdrop-blur-sm bg-white/15 rounded-xl shadow-2xl ">
+            <div className="w-[70rem] h-[18rem] mt-10 backdrop-blur-sm bg-white/15 rounded-xl shadow-2xl ">
                 xvdfvfjk
+            </div>
+            <div className='flex'>
+                <div className='w-[34rem] h-56 m-5 backdrop-blur-sm bg-white/15 rounded-xl shadow-2xl'>
+
+                </div>
+                <div className='w-[34rem] h-56 m-5 backdrop-blur-sm bg-white/15 rounded-xl shadow-2xl'>
+
+                </div>
             </div>
         </div>
     );
