7e980d55de7b9421564c8498bbf8c273b6f0b5d1
somesh <someshsrichandan@gmail.com>
2024-08-16 23:50:38 +0530

    routes add

--- crypto-saga/src/App.js
+++ crypto-saga/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch , Route , Link  } from 'react-router-dom';
 import { Layout , Typography , Space } from 'antd';
 import { Navbar } from './components/Navbar';
+import { HomePage } from './components/HomePage';
 import './App.css'
 const  App = () => {
   return (
@@ -10,7 +11,15 @@ const  App = () => {
         <Navbar/>
       </div>
       <div className='main'>
-
+        <Layout>
+          <div className='routes'>
+            <Switch>
+              <Route exact path='/'>
+                <HomePage/>
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
       </div>
       <div className='footer'>
 
